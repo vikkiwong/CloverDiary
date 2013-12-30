8671d401d3c88c187f6ad6ebca4f068a5659c5a3
vikkiwong <vikkiwong2012@gmail.com>
2013-12-30 16:12:39 +0800

    add miss routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 CloverDiary::Application.routes.draw do
 
   resources :wechat, :only => [:index, :create]
+  resources :messages, :only => [:index, :create]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
