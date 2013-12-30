02da5db3316adede5113b23dbeac3c69e5fe55f9
vikkiwong <vikkiwong2012@gmail.com>
2013-12-30 20:30:23 +0800

    修改问题随机方法

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,8 @@
 class Question < ActiveRecord::Base
   attr_accessible :content, :created_by
 
+  # 随机n个问题
   def self.find_questions(n)
-  	self.last(n)
+  	self.find_by_sql("SELECT * FROM questions order by rand() LIMIT #{n}")
   end
 end
