80c8f64b69da9677be5964aaef7ec5e5969c363a
vikkiwong <vikkiwong2012@gmail.com>
2013-12-30 14:49:59 +0800

    wechat config

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 class WechatController < ApplicationController
+	before_filter :check_weixin_legality, :only => "create"
+	
   def index
     render :text => params[:echostr]
   end
@@ -9,5 +11,11 @@ class WechatController < ApplicationController
   		@text = "欢迎！"
   		render "welcome", :formats => :xml
   	end
+  end
+
+  private
+  def check_weixin_legality
+    array = [WX_TOKEN, params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
   end
 end
