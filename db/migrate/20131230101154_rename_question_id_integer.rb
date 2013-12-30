70f3821ae95316748eb375ea1e7f7f160ceed604
vikkiwong <vikkiwong2012@gmail.com>
2013-12-30 18:15:52 +0800

    补充漏传

---
+++ db/migrate/20131230101154_rename_question_id_integer.rb
@@ -0,0 +1,9 @@
+class RenameQuestionIdIntegerIn < ActiveRecord::Migration
+  def up
+  	rename_column :user_questions, :question_id_integer, :question_id
+  end
+
+  def down
+  	rename_column :user_questions, :question_id, :question_id_integer
+  end
+end
