667c5a049adb1eff762ab170032bd8c46f1ffd84
vikkiwong <vikkiwong2012@gmail.com>
2013-12-31 18:19:47 +0800

    用户回答页面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 class UsersController < ApplicationController
 	def show
-		puts "00000"
+		today  = Date.today
+		qids = Answer.find_by_sql("SELECT * FROM answers WHERE user_id = #{params[:id]} AND created_at > #{today} GROUP BY question_id").collect(&:question_id)
+		@questions = Question.find_all_by_id(qids)
 	end
 end
