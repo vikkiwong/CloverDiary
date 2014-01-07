65e26ab261f623b422c3b368f49a019199de9216
vikkiwong <vikkiwong2012@gmail.com>
2014-01-07 12:02:24 +0800

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find_by_id(params[:id])
 		render "not_found" and return unless @user.present? && @user.open_id == params[:open_id]  # 简单验证下open_id参数
-		@msgs, @w_msgs = @user.msgs(4.days.ago)
+		@msgs, @w_msgs = @user.msgs(Date.today)
 	end
 end
