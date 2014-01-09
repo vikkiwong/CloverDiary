59e0bc966440dc7145a21c92a271d171f303ca41
vikkiwong <vikkiwong2012@gmail.com>
2014-01-09 17:25:09 +0800

    生成tumblr账户

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+	TumblrAccount.create(email: "cloverdiray1314@gmail.com", password: "clover1314", user_name: "cloverdiray", blog_name: "cloverdiary" + (i+1).to_s, 
+		                   blog_url: "cloverdiary" + (i+1).to_s + ".tumblr.com", consumer_key: "tSXD9tLCnN0Zqrl0M0fRTX61kkDMa1zVQgLXP7N6PRzKzc1vbJ", 
+		                   consumer_secret: "TLMLM3JZJsDw4SjTSPeuCX908VYFY9Gox2b1ThCnpkBtwOjub5", oauth_token: "CjOZpQZFyOl24DurrqRbl92fAfsTHYXZdPqMN8nJTeGpRP5dlL", 
+		                   oauth_token_secret: "c1EMiFb8gTRpUvlWd2JPjyhAwpUjVwtCHgZ12CcKHWxa8VQ86x")
+end
